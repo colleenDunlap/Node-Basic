35c15f62b9df2f99f3a84ca195f12cb8db5f5778
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2019-02-07 14:20:08 -0600

    using public pug info

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const server = app.listen(7000, () => {
+const server = app.listen(3000, () => {
 	console.log(`Express Running -> PORT ${server.address().port}`);
 });
 app.get('/', (req, res) => {
   res.render('index', {title:'Homepage'});
 });
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
